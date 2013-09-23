fd003e4ace9a92a17bcc135424a4852b486b3a45
alexflores <axel_f_g@hotmail.com>
2013-09-23 00:25:09 -0400

    Creacion Principal.java y edicion arch pom.xml

---
+++ src/main/java/FizzBuzzTDD/Principal.java
@@ -0,0 +1,12 @@
+package FizzBuzzTDD;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FizzBuzztdd fb = new FizzBuzztdd();
+		System.out.println(fb.generar(3));
+
+	}
+
+}
