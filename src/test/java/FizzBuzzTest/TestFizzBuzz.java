9a7528e6371de998b38494de6294b28b9f728f61
alexflores <axel_f_g@hotmail.com>
2013-09-23 14:55:56 -0400

    Test generaElMismoNumeroSiNoEs3

--- src/test/java/FizzBuzzTest/TestFizzBuzz.java
+++ src/test/java/FizzBuzzTest/TestFizzBuzz.java
@@ -2,16 +2,28 @@ package FizzBuzzTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import FizzBuzzTDD.FizzBuzztdd;
 
 public class TestFizzBuzz {
-
+	private FizzBuzztdd fb;
+	
+	@Before
+	public void setup() {
+		fb = new FizzBuzztdd();
+	}
+	
 	@Test
-	public void imprimeFizzParaNumero3() {
-		FizzBuzztdd fb = new FizzBuzztdd();
+	public void generaFizzSiNumeroEs3() {
 		assertEquals("Fizz",fb.generar(3));
+	}
+	
+	@Test
+	public void generaElMismoNumeroSiNoEs3()
+	{
+		assertEquals("4",fb.generar(4));
 	}
 
 }
