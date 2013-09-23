462ca5ffa350efaf92b77fd891c88783e4f0641b
alexflores <axel_f_g@hotmail.com>
2013-09-23 14:34:07 -0400

    Refactorizacion de archivo FizzBuzztdd.java y generar 100 numeros

--- src/main/java/FizzBuzzTDD/FizzBuzztdd.java
+++ src/main/java/FizzBuzzTDD/FizzBuzztdd.java
@@ -1,16 +1,31 @@
 package FizzBuzzTDD;
 
 public class FizzBuzztdd {
-  public String generar(int numero)
-  {
-	  if(numero%3==0)
-	  {
-		  return "Fizz";
-	  }
-	  if(numero%5==0)
-	  {
-		  return "Buzz";
-	  }
-	  return Integer.toString(numero);
+	
+  private static final int _FIZZ = 3;
+  private static final int _BUZZ = 5;
+  private static final String _MENSAJE_BUZZ = "Buzz";
+  private static final String _MENSAJE_FIZZ = "Fizz";
+  private static final String _MENSAJE_FIZZ_BUZZ = "FizzBuzz";
+	
+  public String generar(int numero) {
+		if (esMultiploDe(numero, _FIZZ)&&esMultiploDe(numero, _BUZZ))
+			return _MENSAJE_FIZZ_BUZZ;
+		if (esMultiploDe(numero, _FIZZ))
+			return _MENSAJE_FIZZ;
+		if (esMultiploDe(numero, _BUZZ))
+			return _MENSAJE_BUZZ;
+		return Integer.toString(numero);			
+  }
+  
+  private boolean esMultiploDe(int numero, int multiplo) {
+		return numero % multiplo == 0;
+  }
+  
+  public String imprimir(int maximo) {
+		String resultado = "";
+		for (int i = 1; i <= maximo; i++)
+			resultado = resultado + generar(i) + "\n";
+		return resultado;
   }
 }
--- src/main/java/FizzBuzzTDD/Principal.java
+++ src/main/java/FizzBuzzTDD/Principal.java
@@ -3,9 +3,8 @@ package FizzBuzzTDD;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		FizzBuzztdd fb = new FizzBuzztdd();
-		System.out.println(fb.generar(3));
+		System.out.println(fb.imprimir(100));
 
 	}
 
